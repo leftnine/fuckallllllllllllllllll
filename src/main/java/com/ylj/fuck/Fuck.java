3cfb736c5036c76cbb13d073b129567fa98f2034
Leftnine <864963754@qq.com>
2020-11-20 12:14:28 +0800

    init

---
+++ src/main/java/com/ylj/fuck/Fuck.java
@@ -0,0 +1,28 @@
+package com.ylj.fuck;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Fuck.MOD_ID, name = Fuck.NAME, version = Fuck.VERSION)
+public class Fuck {
+    public static final String MOD_ID = "fuck";
+    public static final String NAME = "Fuck all";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
